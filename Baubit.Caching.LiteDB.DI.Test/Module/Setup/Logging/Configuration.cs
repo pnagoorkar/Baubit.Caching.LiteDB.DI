40e32a0f0c79045337803528ab1320e120b7e02b
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-12-19 09:23:18 -0500

    Upgraded to latest Baubit packages to harden config driven module loading and standardize naming conventions

--- Baubit.Caching.LiteDB.DI.Test/Module/Setup/Logging/Configuration.cs
+++ Baubit.Caching.LiteDB.DI.Test/Module/Setup/Logging/Configuration.cs
@@ -3,7 +3,7 @@ namespace Baubit.Caching.LiteDB.DI.Test.Module.Setup.Logging
     /// <summary>
     /// Configuration for the logging module.
     /// </summary>
-    public class Configuration : Baubit.DI.AConfiguration
+    public class Configuration : Baubit.DI.Configuration
     {
     }
 }
